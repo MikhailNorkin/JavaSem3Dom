7117f1dbb3ddc92d08d908cce39d3f59147cfc2d
MikhailNorkin <mvnorkin@gmail.com>
2022-11-10 15:34:39 +0300

    Task2 is complete

---
+++ Task2.java
@@ -0,0 +1,23 @@
+
+package Tasks;
+import java.util.ArrayList;
+import java.util.List;
+
+//Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+public class Task2 {
+    public static void main(String[] args) {
+        List listA = new ArrayList();
+        listA.add(1);
+        listA.add(2);
+        listA.add(3);
+        System.out.println(listA);
+        int max = listA.size()-1;
+        for (int i = max; i >= 0; i--) {
+            if (i%2 != 0){
+                listA.remove(i);
+            }
+        }
+        System.out.println(listA);
+    }    
+}
